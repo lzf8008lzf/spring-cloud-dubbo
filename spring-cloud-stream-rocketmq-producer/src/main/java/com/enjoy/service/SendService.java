ffa72b5746d07155c3acb3670a9e96d151047c52
LiZhaofu <lzf8008lzf@163.com>
2020-05-27 17:51:07 +0800

    rocketmq的延迟消息

--- spring-cloud-stream-rocketmq-producer/src/main/java/com/enjoy/service/SendService.java
+++ spring-cloud-stream-rocketmq-producer/src/main/java/com/enjoy/service/SendService.java
@@ -1,11 +1,13 @@
 package com.enjoy.service;
 
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.Output;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Service;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -20,6 +22,7 @@ import java.util.UUID;
 
 //这个注解给我们绑定消息通道的，Source是Stream给我们提供的，可以点进去看源码，可以看到output和input,这和配置文件中的output，input对应的。
 @EnableBinding(Source.class)
+@Service
 public class SendService {
 
     @Autowired
@@ -36,6 +39,7 @@ public class SendService {
         Date d = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         System.out.println("当前时间：" + sdf.format(d));
-        source.output().send(MessageBuilder.withPayload(msg).setHeader("x-delay",3000).build());
+        //messageDelayLevel=1s 5s 10s 30s 1m 2m 3m 4m 5m 6m 7m 8m 9m 10m 20m 30m 1h 2h
+        source.output().send(MessageBuilder.withPayload(msg).setHeader("DELAY",3).build());
     }
 }
