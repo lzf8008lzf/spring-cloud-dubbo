1e8f56d2c90b76e7666e02b35398eb4031428f3f
LiZhaofu <lzf8008lzf@163.com>
2021-03-25 09:10:45 +0800

    设置复杂表头

--- spring-cloud-jwt/src/main/java/com/enjoy/model/AdBillInfoPO.java
+++ spring-cloud-jwt/src/main/java/com/enjoy/model/AdBillInfoPO.java
@@ -14,60 +14,55 @@ import java.util.Date;
 @Data
 public class AdBillInfoPO implements Serializable {
 
-    /**
-     * 广告ID
-     */
-    @ExcelProperty(value = "广告ID",index = 0)
-    private Integer advertId;
 
     /**
      * 广告标题
      */
-    @ExcelProperty(value = "广告标题",index = 1)
+    @ExcelProperty(value = "广告标题",index = 0)
     private String adTitle;
 
     /**
      * 媒体名称
      */
-    @ExcelProperty(value = "媒体名称",index = 2)
+    @ExcelProperty(value = "媒体名称",index = 1)
     private String adMedium;
 
     /**
      * 广告位
      */
-    @ExcelProperty(value = "广告位",index = 3)
+    @ExcelProperty(value = "广告位",index = 2)
     private String adName;
 
     /**
      * 广告类型
      */
-    @ExcelProperty(value = "广告类型",index = 4)
+    @ExcelProperty(value = "采购类型",index = 3)
     private String advertType;
 
     /**
      * 账单日期
      */
-    @ExcelProperty(value = "账单日期",index = 5)
+    @ExcelProperty(value = "执行时间",index = 4)
     private Date billDate;
 
     /**
      * 曝光量
      */
-    @ExcelProperty(value = "曝光量",index = 6)
+    @ExcelProperty(value = "曝光量",index = 5)
     private Integer impressions;
 
     /**
      * CPM单价
      */
-    @ExcelProperty(value = "CPM单价",index = 7)
+    @ExcelProperty(value = "CPM单价/元",index = 6)
     private Long cpm;
 
     /**
      * 结算价格
      */
-    @ExcelProperty(value = "结算价格",index = 8)
+    @ExcelProperty(value = "日净价",index = 7)
     private BigDecimal amount;
 
     private static final long serialVersionUID = 1L;
-    
+
 }
--- spring-cloud-jwt/src/test/java/com/enjoy/EasyExcelTest.java
+++ spring-cloud-jwt/src/test/java/com/enjoy/EasyExcelTest.java
@@ -1,11 +1,20 @@
 package com.enjoy;
 
+import cn.hutool.core.date.DatePattern;
+import cn.hutool.core.date.DateUtil;
 import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.write.metadata.WriteSheet;
+import com.alibaba.excel.write.metadata.style.WriteCellStyle;
+import com.alibaba.excel.write.metadata.style.WriteFont;
+import com.alibaba.excel.write.style.HorizontalCellStyleStrategy;
 import com.enjoy.component.CellWriteComponent;
 import com.enjoy.model.AdBillInfoPO;
 import com.enjoy.utils.FileUtil;
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -22,36 +31,168 @@ import java.util.List;
 public class EasyExcelTest {
 
     public static void main(String[] args){
-        // 方法2 如果写到不同的sheet 同一个对象
-        String fileName = FileUtil.getPath() + "repeatedWrite" + System.currentTimeMillis() + ".xlsx";
+        writeFileHeader();
+    }
+
+    private static void writeStream(){
+//        EasyExcel.write(response.getOutputStream(), AdBillInfoPO.class).useDefaultStyle(false).head(AdBillInfoPO.class).registerWriteHandler(new CellWriteComponent()).sheet(sheetName).doWrite(data);
+    }
+
+    private static List<List<String>> getHeader(String title, String agentName, String billDate) {
+        /**
+         * 打算展示成如下样子
+         * |代理商名称 XXX
+         * |月份 XX月
+         * |账单信息
+         * |广告标题|媒体名称|广告位|采购类型|执行时间|曝光量|CPM单价/元|日净价
+         */
+        String agent="代理商名称";
+        String month="月份";
+        List<List<String>> list = new ArrayList<List<String>>();
+
+        List<String> head1 = new ArrayList<String>();
+        head1.add(agent);
+        head1.add(month);
+        head1.add(title);
+        head1.add("广告标题");
+
+        List<String> head2 = new ArrayList<String>();
+        head2.add(agentName);
+        head2.add(billDate);
+        head2.add(title);
+        head2.add("媒体名称");
+
+        List<String> head3 = new ArrayList<String>();
+        head3.add(agentName);
+        head3.add(billDate);
+        head3.add(title);
+        head3.add("广告位");
+
+        List<String> head4 = new ArrayList<String>();
+        head4.add(agentName);
+        head4.add(billDate);
+        head4.add(title);
+        head4.add("采购类型");
+
+        List<String> head5 = new ArrayList<String>();
+        head5.add(agentName);
+        head5.add(billDate);
+        head5.add(title);
+        head5.add("执行时间");
+
+        List<String> head6 = new ArrayList<String>();
+        head6.add(agentName);
+        head6.add(billDate);
+        head6.add(title);
+        head6.add("曝光量");
+
+        List<String> head7 = new ArrayList<String>();
+        head7.add(agentName);
+        head7.add(billDate);
+        head7.add(title);
+        head7.add("CPM单价/元");
+
+        List<String> head8 = new ArrayList<String>();
+        head8.add(agentName);
+        head8.add(billDate);
+        head8.add(title);
+        head8.add("日净价");
+
+        list.add(head1);
+        list.add(head2);
+        list.add(head3);
+        list.add(head4);
+        list.add(head5);
+        list.add(head6);
+        list.add(head7);
+        list.add(head8);
+
+        return list;
+    }
+
+    private static void writeFileHeader(){
+        List<String> excludeColumns = new ArrayList<>();
+        excludeColumns.add("memo");
+
+        String fileName = FileUtil.getPath() + "repeatedWrite" + DateUtil.format(new Date(), DatePattern.PURE_DATE_PATTERN) + ".xlsx";
         ExcelWriter excelWriter = null;
 
         try {
             // 这里 指定文件
-            excelWriter = EasyExcel.write(fileName, AdBillInfoPO.class).useDefaultStyle(false).registerWriteHandler(new CellWriteComponent()).build();
-            // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来。这里最终会写到5个sheet里面
-            for (int i = 0; i < 5; i++) {
-                // 每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样
-                WriteSheet writeSheet = EasyExcel.writerSheet(i, "模板" + i).build();
-                // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
-                List<AdBillInfoPO> data = data();
-                excelWriter.write(data, writeSheet);
+            excelWriter = EasyExcel.write(fileName).useDefaultStyle(false)
+                    .registerWriteHandler(new CellWriteComponent())
+                    .registerWriteHandler(getStyleStrategy())
+                    .build();
 
-                // 合计部分
-                List<List<String>> totalListList = new ArrayList<List<String>>();
-                List<String> totalList = new ArrayList<String>();
-                totalListList.add(totalList);
-                totalList.add("合计：");
-                totalList.add("80000000000");
-                excelWriter.write(totalListList, writeSheet);
+            WriteSheet writeSheet = EasyExcel.writerSheet( "模板" ).head(getHeader("账单信息","悦享商城App运营部","2月")).build();
+            List<AdBillInfoPO> data = data();
+            excelWriter.write(data, writeSheet);
+            // 合计部分
+            List<List<String>> totalListList = new ArrayList<List<String>>();
+            List<String> totalList = new ArrayList<String>();
+            totalListList.add(totalList);
+            for(int j=0;j<6;j++) {
+                totalList.add("");
             }
+            totalList.add("合计：");
+            totalList.add("80000000000");
+            excelWriter.write(totalListList, writeSheet);
+
         } finally {
             // 千万别忘记finish 会帮忙关闭流
             if (excelWriter != null) {
                 excelWriter.finish();
             }
         }
+    }
 
+    private static void writeFile(){
+        String fileName = FileUtil.getPath() + "repeatedWrite" + DateUtil.format(new Date(), DatePattern.PURE_DATE_PATTERN) + ".xlsx";
+        ExcelWriter excelWriter = null;
+
+        try {
+            // 这里 指定文件
+            excelWriter = EasyExcel.write(fileName, AdBillInfoPO.class).useDefaultStyle(false).registerWriteHandler(new CellWriteComponent())
+                    .registerWriteHandler(getStyleStrategy()).build();
+
+            WriteSheet writeSheet = EasyExcel.writerSheet( "模板" ).build();
+            List<AdBillInfoPO> data = data();
+            excelWriter.write(data, writeSheet);
+            // 合计部分
+            List<List<String>> totalListList = new ArrayList<List<String>>();
+            List<String> totalList = new ArrayList<String>();
+            totalListList.add(totalList);
+            for(int j=0;j<7;j++) {
+                totalList.add("");
+            }
+            totalList.add("合计：");
+            totalList.add("80000000000");
+            excelWriter.write(totalListList, writeSheet);
+//            // 去调用写入,这里我调用了五次，实际使用时根据数据库分页的总的页数来。这里最终会写到5个sheet里面
+//            for (int i = 0; i < 5; i++) {
+//                // 每次都要创建writeSheet 这里注意必须指定sheetNo 而且sheetName必须不一样
+//                WriteSheet writeSheet = EasyExcel.writerSheet(i, "模板" + i).build();
+//                // 分页去数据库查询数据 这里可以去数据库查询每一页的数据
+//                List<AdBillInfoPO> data = data();
+//                excelWriter.write(data, writeSheet);
+//
+//                // 合计部分
+//                List<List<String>> totalListList = new ArrayList<List<String>>();
+//                List<String> totalList = new ArrayList<String>();
+//                totalListList.add(totalList);
+//                for(int j=0;j<7;j++) {
+//                    totalList.add("");
+//                }
+//                totalList.add("合计：");
+//                totalList.add("80000000000");
+//                excelWriter.write(totalListList, writeSheet);
+//            }
+        } finally {
+            // 千万别忘记finish 会帮忙关闭流
+            if (excelWriter != null) {
+                excelWriter.finish();
+            }
+        }
     }
 
     private static List<AdBillInfoPO> data() {
@@ -69,5 +210,46 @@ public class EasyExcelTest {
             list.add(data);
         }
         return list;
+    }
+
+    private static HorizontalCellStyleStrategy getStyleStrategy() {
+        // 头的策略
+        WriteCellStyle headWriteCellStyle = new WriteCellStyle();
+        // 设置对齐
+        headWriteCellStyle.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        // 背景色, 设置为白色，也是默认颜色
+        headWriteCellStyle.setFillForegroundColor(IndexedColors.WHITE.getIndex());
+        // 字体
+        WriteFont headWriteFont = new WriteFont();
+        headWriteFont.setFontHeightInPoints((short) 12);
+        headWriteCellStyle.setWriteFont(headWriteFont);
+
+        // 内容的策略
+        WriteCellStyle contentWriteCellStyle = new WriteCellStyle();
+        // 这里需要指定 FillPatternType 为FillPatternType.SOLID_FOREGROUND 不然无法显示背景颜色.头默认了 FillPatternType所以可以不指定
+        // contentWriteCellStyle.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+        // contentWriteCellStyle.setFillPatternType(FillPatternType.SOLID_FOREGROUND);
+
+        // 背景绿色
+        //contentWriteCellStyle.setFillForegroundColor(IndexedColors.GREEN.getIndex());
+        // 字体策略
+        WriteFont contentWriteFont = new WriteFont();
+        contentWriteFont.setFontHeightInPoints((short) 12);
+        contentWriteCellStyle.setWriteFont(contentWriteFont);
+        //设置 自动换行
+        contentWriteCellStyle.setWrapped(true);
+        //设置 垂直居中
+        contentWriteCellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        //设置 水平居中
+        // contentWriteCellStyle.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        //设置边框样式
+        contentWriteCellStyle.setBorderLeft(BorderStyle.THIN);
+        contentWriteCellStyle.setBorderTop(BorderStyle.THIN);
+        contentWriteCellStyle.setBorderRight(BorderStyle.THIN);
+        contentWriteCellStyle.setBorderBottom(BorderStyle.THIN);
+
+        // 这个策略是 头是头的样式 内容是内容的样式 其他的策略可以自己实现
+        HorizontalCellStyleStrategy horizontalCellStyleStrategy = new HorizontalCellStyleStrategy(headWriteCellStyle, contentWriteCellStyle);
+        return horizontalCellStyleStrategy;
     }
 }
