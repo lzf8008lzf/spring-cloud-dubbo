3078aeb099b7b556f370bdf8bc2f5dad0a3d18d4
LiZhaofu <lzf8008lzf@163.com>
2021-04-26 14:42:34 +0800

    添加对象比较工具类

---
+++ spring-cloud-mybatisplus/src/main/java/com/enjoy/core/utils/CompareObejct.java
@@ -0,0 +1,12 @@
+package com.enjoy.core.utils;/**
+* @program: spring-cloud-dubbo
+*
+* @description: 
+*
+* @author: LiZhaofu
+*
+* @create: 2021-04-12 17:04
+**/
+
+public class CompareObejct {
+}
