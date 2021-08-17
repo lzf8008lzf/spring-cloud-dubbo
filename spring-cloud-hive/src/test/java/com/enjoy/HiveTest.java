5da1d1235764008a2d7eadb5f329098e97785ace
LiZhaofu <lzf8008lzf@163.com>
2021-08-17 17:02:06 +0800

    增加hive示例

---
+++ spring-cloud-hive/src/test/java/com/enjoy/HiveTest.java
@@ -0,0 +1,28 @@
+package com.enjoy;
+
+import cn.hutool.json.JSONUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Map;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = HiveApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Slf4j
+public class HiveTest {
+    @Autowired
+    private JdbcTemplate hiveJdbcTemplate;
+
+    @Test
+    public void hiveTest(){
+        String sql="select * from test10";
+        List<Map<String, Object>> maps = hiveJdbcTemplate.queryForList(sql);
+        System.out.println(JSONUtil.toJsonStr(maps));
+    }
+}
